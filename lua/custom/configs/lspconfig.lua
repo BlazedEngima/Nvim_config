66e261c221cd160237c8d151dee86f0b27329cf9
Blazed <samueltheofie@gmail.com>
2024-04-07 02:49:45 +0700

    Added clangd options

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -7,13 +7,33 @@ local lspconfig = require("lspconfig")
 lspconfig.clangd.setup {
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
+
+    if client.server_capabilities.inlayHintProvider then
+      vim.g.inlay_hints_visible = true
+      vim.lsp.inlay_hint(bufnr, true)
+    else
+      print("No inlay hints available")
+    end
+
     on_attach(client, bufnr)
   end,
 
   capabilities = capabilities,
     cmd = {
       "clangd",
-      "--offset-encoding=utf-16"
+      "--offset-encoding=utf-16",
+      "--background-index",
+      "--clang-tidy",
+      "--header-insertion=iwyu",
+      "--completion-style=detailed",
+      "--function-arg-placeholders",
+      "-j=4",
+      "--fallback-style=llvm",
+    },
+    init_options = {
+      usePlaceholders = true,
+      completeUnimported = true,
+      clangdFileStatus = true,
     },
 }
 
