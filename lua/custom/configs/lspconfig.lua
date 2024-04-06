7630a137deccc9c55e9681634a7ac6a0adecca9d
Blazed <samueltheofie@gmail.com>
2024-04-07 01:56:38 +0700

    Changed offset-encoding

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -9,6 +9,12 @@ lspconfig.clangd.setup {
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client, bufnr)
   end,
+
   capabilities = capabilities,
+    cmd = {
+      "clangd",
+      "--offset-encoding=utf-16"
+    },
 }
 
+lspconfig.cmake.setup{}
